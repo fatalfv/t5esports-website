a3c32181f5d333d2201950877062adb40dbde747
recon <zadks4130@gmail.com>
2026-08-24 17:18:30 +0100

    Update roster.js

--- roster.js
+++ roster.js
@@ -19,7 +19,7 @@ const rosterPlayers = [
         name: "Fatal",
         role: "Owner",
         region: "EU",
-        image: "images/fatal.jpeg",
+        image: "https://github.com/fatalfv/t5esports-website/blob/main/images/fatal.jpeg?raw=true",
         discord: "https://discord.gg/t5esports"
     },
 
